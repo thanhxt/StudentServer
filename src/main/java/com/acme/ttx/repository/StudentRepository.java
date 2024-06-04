d8112ab75ef204f783cde41cf4fbe6e4a9ba1c2a
thanhxt <thanhtran_xuan@hotmail.de>
2024-06-04 20:54:24 +0200

    edited Repository with Jakarta Persistance query Language and Spring Data JPA

--- src/main/java/com/acme/ttx/repository/StudentRepository.java
+++ src/main/java/com/acme/ttx/repository/StudentRepository.java
@@ -1,230 +1,101 @@
 package com.acme.ttx.repository;
 
-import com.acme.ttx.entity.ModuleType;
+
+import com.acme.ttx.entity.Adresse;
 import com.acme.ttx.entity.Student;
-import jakarta.validation.constraints.NotNull;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.OptionalInt;
-import java.util.UUID;
-import java.util.stream.IntStream;
 import lombok.NonNull;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import static com.acme.ttx.repository.DB.STUDENTEN;
-import static java.util.Collections.emptyList;
-import static java.util.UUID.randomUUID;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static com.acme.ttx.entity.Student.ADRESSE_GRAPH;
+import static com.acme.ttx.entity.Student.ADRESSE_GUTHABEN_GRAPH;
+
 
 /**
- * Mock-Klasse für den DB-Zugriff.
+ * Repository für den DB-Zugriff
  */
 @Repository
-@Slf4j
-@SuppressWarnings("PublicConstructor")
-public class StudentRepository {
+public interface StudentRepository extends JpaRepository<Student, UUID>, JpaSpecificationExecutor<Student> {
+    @EntityGraph(ADRESSE_GRAPH)
+    @NonNull
+    List<Student> findAll();
+
+    @EntityGraph(ADRESSE_GRAPH)
+    @NonNull
+    @Override
+    List<Student> findAll(@NonNull Specification<Student> spec);
+
+    @EntityGraph(ADRESSE_GRAPH)
+    @NonNull
+    @Override
+    Optional<Student> findById(@NonNull UUID id);
 
     /**
-     * Alle Studenten als Collection ermitteln, wie sie später auch in der DB vorkommen.
+     * Student einschließlich Guthaben anhand der ID Suchen.
      *
-     * @return Alle Studenten
+     * @param id Student ID
+     * @return gefundenen Studenten
      */
-    public Collection<Student> findAll() {
-        return STUDENTEN;
-    }
+    @Query("""
+        SELECT DISTINCT s
+        FROM #{#entityName} s
+        WHERE s.id = :id
+    """)
+    @EntityGraph(ADRESSE_GUTHABEN_GRAPH)
+    @NonNull
+    Optional<Student> findByIdFetchGuthaben(UUID id);
 
     /**
-     * Student zu einer gegebenen Matrikelnummer in der DB ermitteln.
+     * Student zu gegebener E-Mail-Adresse aus der DB ermitteln.
      *
-     * @param id Matrikelnummer für die Suche
-     * @return Gefundener Student oder leeres Optional
+     * @param email E-Mail-Adresse des gesuchten Studenten
+     * @return Optional mit gefundenen Studenten oder leeres Optional
      */
-    public Optional<Student> findById(final UUID id) {
-        log.debug("findById: id={}", id);
-        final var result = STUDENTEN.stream()
-            .filter(student -> Objects.equals(student.getId(), id))
-            .findFirst();
-        log.debug("findById: {}", result);
-        return result;
-    }
+    @Query("""
+        SELECT s
+        FROM #{#entityName} s
+        WHERE lower(s.email) LIKE concat(lower(:email), '%')
+    """)
+    @EntityGraph(ADRESSE_GRAPH)
+    Optional<Student> findByEmail(String email);
+
+    /**
+     * Abfrage, ob es einen Studenten mit gegebener E-Mail existiert.
+     * @param email E-Mail-Adresse für die Suche
+     * @return true, falls es solchen Studenten gibt, sonst false
+     */
+    @SuppressWarnings("BooleanMethodNameMustStartWithQuestion")
+    boolean existsByEmail(String email);
 
     /**
      * Studenten anhand des Nachnamen suchen.
      *
-     * @param nachname Der Nachname des gesuchen Studenten
-     * @return Die gefundenen Studenten oder eine leere Collection.
+     * @param nachname der (Teil-) Nachname des gesuchten Studenten
+     * @return den gefundenen Studenten oder eine leere Collection
      */
-    public Collection<Student> findByNachname(final @NonNull String nachname) {
-        log.debug("findByNachname: nachname={}", nachname);
-        final var result = STUDENTEN.stream()
-            .filter(student -> Objects.equals(student.getNachname(), nachname))
-            .toList();
-        log.debug("findByNachname: student={}", result);
-        return result;
-    }
+    @Query("""
+        SELECT DISTINCT s
+        FROM #{#entityName} s
+        WHERE lower(s.nachname) LIKE concat ('%', lower(:nachname), '%')
+        GROUP BY s.nachname
+        """)
+    @EntityGraph(ADRESSE_GRAPH)
+    @NonNull
+    List<Student> findByNachname(CharSequence nachname);
 
-    /**
-     * Studenten anhand des Namen suchen.
-     *
-     * @param name Der Name des gesuchten Studenten
-     * @return Die gefundenen Studenten oder eine Leere Collection
-     */
-    public Collection<Student> findByName(final String name) {
-        log.debug("findByName: name: {}", name);
-        final var result = STUDENTEN.stream()
-            .filter(student -> Objects.equals(student.getName(), name))
-            .toList();
-        log.debug("findByName: student={}", result);
-        return result;
-    }
-
-    /**
-     * Studenten zu gegebener Emailadresse aus der DB ermitteln.
-     *
-     * @param email Emailadresse für die Suche
-     * @return Gefundener Student oder leeres Optional
-     */
-    public Optional<Student> findByEmail(final String email) {
-        log.debug("findByEmail: email: {}", email);
-        final var result = STUDENTEN.stream()
-            .filter(student -> student.getEmail().contentEquals(email))
-            .findFirst();
-        log.debug("findByEmail: {}", result);
-        return result;
-    }
-
-    /**
-     * Abfrage, ob es einen Studenten mit gegebener E-Mail-Adresse gibt.
-     *
-     * @param email E-Mail-Adresse für die Suche
-     * @return true, falls es einen solchen Studenten gibt.
-     */
-    public boolean isEmailExisting(final String email) {
-        log.debug("isEmailExisting: email={}", email);
-        final var result = STUDENTEN.stream()
-            .filter(student -> student.getEmail().contentEquals(email))
-            .count();
-        log.debug("isEmailExisting: result={}", result);
-        return result > 0L;
-    }
-
-    /**
-     * Studenten zu gegebenen Semester aus der DB ermitteln.
-     *
-     * @param semesterStr Semester als String
-     * @return Gefundene Studenten oder leere Collection
-     */
-    public Collection<Student> findBySemester(final String semesterStr) {
-        log.debug("findBySemester: semesterStr: {}", semesterStr);
-        final var result = STUDENTEN.stream()
-            .filter(student -> Objects.equals(student.getSemester().getValue(), semesterStr))
-            .toList();
-        log.debug("findBySemester: {}", result);
-        return result;
-    }
-
-    /**
-     * Studenten zu gegebenen Modulen aus der DB ermitteln.
-     *
-     * @param moduleStr Modul für die Suche
-     * @return Gefundene Studenten oder leere Collection
-     */
-    private @NotNull Collection<Student> findByModule(final Collection<String> moduleStr) {
-        log.debug("findByModule: moduleStr: {}", moduleStr);
-        final var module = moduleStr.stream()
-            .map(ModuleType::of)
-            .toList();
-        if (module.contains(null)) {
-            return emptyList();
-        }
-
-        log.trace("findByModule: module={}", module);
-        final var studenten = STUDENTEN.stream()
-            .filter(student -> {
-                final Collection<ModuleType> studentModule = new HashSet<>(student.getModule());
-                return studentModule.containsAll(module);
-            })
-            .toList();
-        log.debug("findByModule: student={}", studenten);
-        return studenten;
-    }
-
-    /**
-     * Kunden anhand von Suchkriterien ermitteln. Z.B. mit GET
-     * <a href="https://localhost:8080/api?modul=SWA&modul=MA1">...</a>
-     *
-     * @param suchkriterien Suchkriterien.
-     * @return Gefundene Studenten oder leere Collection.
-     */
-    @SuppressWarnings("ReturnCount")
-    public @NotNull Collection<Student> find(final Map<String, ? extends  List<String>> suchkriterien) {
-        log.debug("find: suchkriterien={}", suchkriterien);
-
-        if (suchkriterien.isEmpty()) {
-            return findAll();
-        }
-
-        for (final var entry : suchkriterien.entrySet()) {
-            switch (entry.getKey()) {
-                case "email" -> {
-                    final var student = findByEmail(entry.getValue().getFirst()).orElse(null);
-                    return student == null ? emptyList() : List.of(student);
-                }
-                case "modul" -> {
-                    return findByModule(entry.getValue());
-                }
-                case "name" -> {
-                    return findByName(entry.getValue().getFirst());
-                }
-                case "nachname" -> {
-                    return findByNachname(entry.getValue().getFirst());
-                }
-                case "semester" -> {
-                    return findBySemester(entry.getValue().getFirst());
-                }
-                default -> {
-                    log.debug("find: ungueltiges suchkriterien={}", entry.getKey());
-                    return emptyList();
-                }
-            }
-        }
-
-        return emptyList();
-    }
-
-    /**
-     * Einen Studenten anlegen.
-     *
-     * @param student Das Objekt des neu anzulegenden Studenten
-     * @return Der neu angelegte Student mit generierter Matrikelnummer
-     */
-    public @NonNull Student create(final @NonNull Student student) {
-        log.debug("create: {}", student);
-        student.setId(randomUUID());
-        STUDENTEN.add(student);
-        log.debug("created: {}", student);
-        return student;
-    }
-
-    /**
-     * Einen vorhandenen Studenten aktualisieren.
-     *
-     * @param student Das Obkelt mit den neuen Daten
-     */
-    public void update(final @NonNull Student student) {
-        log.debug("update: {}", student);
-        final OptionalInt index = IntStream
-            .range(0, STUDENTEN.size())
-            .filter(i -> Objects.equals(STUDENTEN.get(i).getId(), student.getId()))
-            .findFirst();
-        log.trace("update: index={}", student);
-        if (index.isEmpty()) {
-            return;
-        }
-        STUDENTEN.set(index.getAsInt(), student);
-        log.debug("update: {}", student);
-    }
+    @Query("""
+        SELECT s
+        FROM #{#entityName} s
+        WHERE    lower(s.nachname) LIKE concat(lower(:prefix), '%')
+        ORDER BY s.nachname
+        """)
+    List<Student> findNachnamenByPrefix(String prefix);
 }
