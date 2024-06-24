33dbe1e73533d8d3d35d979cf59377d211b37f54
thanhxt <thanhtran_xuan@hotmail.de>
2024-06-24 17:12:05 +0200

    added Get Request with ID as Path-Parameter

--- src/main/java/com/acme/ttx/controller/StudentGetController.java
+++ src/main/java/com/acme/ttx/controller/StudentGetController.java
@@ -1,29 +1,38 @@
 package com.acme.ttx.controller;
 
+
 import com.acme.ttx.entity.Student;
-import com.acme.ttx.service.StudentReadServiceOld;
+import com.acme.ttx.security.JwtService;
+import com.acme.ttx.service.StudentReadService;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.servlet.http.HttpServletRequest;
-import java.util.Collection;
-import java.util.UUID;
-import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.LinkRelation;
-import org.springframework.util.MultiValueMap;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import static com.acme.ttx.controller.StudentGetController.REST_PATH;
+import org.springframework.web.client.HttpClientErrorException.Unauthorized;
+
+import java.util.Objects;
+import java.util.Optional;
+import java.util.UUID;
+
+import static com.acme.ttx.controller.StudentGetControllerOld.REST_PATH;
+import static org.apache.tomcat.websocket.Constants.UNAUTHORIZED;
 import static org.springframework.hateoas.MediaTypes.HAL_JSON_VALUE;
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.http.HttpStatus.NOT_MODIFIED;
+import static org.springframework.http.ResponseEntity.ok;
+import static org.springframework.http.ResponseEntity.status;
 
 /**
  * Eine Controller-Klasse bildet die Rest-Schnittstelle.
@@ -36,7 +45,6 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 @Slf4j
 @SuppressWarnings("java:S1075")
 public class StudentGetController {
-
     /**
      * Basispfad für die Rest-Schnittstelle.
      */
@@ -47,89 +55,63 @@ public class StudentGetController {
      */
     public static final String ID_PATTERN = "[\\da-f]{8}-[\\da-f]{4}-[\\da-f]{4}-[\\da-f]{4}-[\\da-f]{12}";
 
-    /**
-     * Pfad um Matrikelnummer zu suchen.
-     */
-    private final StudentReadServiceOld service;
+    private final StudentReadService service;
+    private final JwtService jwtService;
     private final UriHelper uriHelper;
 
     /**
-     * Suche alle Studenten-Matrikelnummer als Pfadparameter.
+     * Suche anhand der Student-ID als Pfad-Parameter.
      *
-     * @return Alle Studenten
-     */
-    @GetMapping(value = "id", produces = APPLICATION_JSON_VALUE)
-    @Operation(summary = "Suche alle Studenten", tags = "Suchen")
-    @ApiResponse(responseCode = "200", description = "Studenten gefunden")
-    @ApiResponse(responseCode = "404", description = "kein Studenten gefunden ")
-    public Collection<Student> getAll() {
-        return service.findAllStudents();
-    }
-
-    /**
-     * Suche alle Studenten-Matrikelnummer als Queryparameter.
-     *
-     * @param suchkriterien Query-Parameter als Map.
+     * @param id ID des suchenden Studenten
+     * @param version Versionsnummer aus dem Header if-None-Match
      * @param request Das Request-Objekt, um Links für HATEOAS zu erstellen.
-     * @return Gefundenen Studenten als Collection.
+     * @param jwt JWT für Security
+     * @return Ein Response mit dem Statuscode 200 und dem gefundenen Studenten mit Atom-Links oder Statuscode 404.
      */
-    @GetMapping(produces = HAL_JSON_VALUE)
-    @Operation(summary = "Suche mit Suchkriterien", tags = "Suchen")
-    @ApiResponse(responseCode = "200", description = "Collection mit den Studenten")
-    @ApiResponse(responseCode = "404", description = "Keine Studenten gefunden")
-    public CollectionModel<StudentModel> get(
-        @RequestParam @NonNull final MultiValueMap<String, String> suchkriterien,
-        final HttpServletRequest request) {
-
-        log.debug("get: suchkriterien={}", suchkriterien);
-
-        final var baseUri = uriHelper.getBaseUri(request).toString();
-
-        final var models = service.find(suchkriterien)
-                .stream()
-                .map(student -> {
-                    final var model = new StudentModel(student);
-                    model.add(Link.of(baseUri + '/' + student.getId()));
-                    return model;
-                })
-                    .toList();
-
-        log.debug("get: {}", models);
-        return CollectionModel.of(models);
-    }
-
-    /**
-     * Suche alle Studenten-Matrikelnummer als Pfadparameter.
-     *
-     * @param id Matrikelnummer des gesuchten Studenten
-     * @param request Das Request-Objekt, um Links für HATEOAS zu erstellen.
-     * @return Gefundener Student
-     */
-    @SuppressWarnings("java:S6856")
     @GetMapping(path = "{id:" + ID_PATTERN + "}", produces = HAL_JSON_VALUE)
-    @Operation(summary = "Suche Studenten anhand der Id", tags = "Suchen")
+    @Operation(summary = "Suche mit der Student-ID", tags = "Suchen")
     @ApiResponse(responseCode = "200", description = "Student gefunden")
     @ApiResponse(responseCode = "404", description = "Student nicht gefunden")
-    StudentModel getStudentByMatrikelnummer(@PathVariable final UUID id,
-                                            final HttpServletRequest request) {
-        log.debug("getStudentById: id={}, Thread={}", id, Thread.currentThread());
+    ResponseEntity<StudentModel> getById(
+        @PathVariable final UUID id,
+        @RequestHeader final Optional<String> version,
+        final HttpServletRequest request,
+        @AuthenticationPrincipal final Jwt jwt
+    ) {
+        final var username = jwtService.getUsername(jwt);
+        log.debug("getById: id={}, version={}, username={}", id, version, username);
 
-        //Geschäftslogik bzw. Anwendungskern
-        final var student = service.findStudentById(id);
+        if(username == null) {
+            log.error("Trotz Spring Security wurde getById() ohne Benutzername im JWT aufgerufen");
+            return status(UNAUTHORIZED).build();
+        }
+        final var rollen = jwtService.getRollen(jwt);
+        log.trace("getById: rollen={}", rollen);
 
-        //HATEOAS
+        final var student = service.findById(id, username, rollen, false);
+        log.trace("getById: {}", student);
+
+        final var currentVersion = "\"" + student.getVersion() + '"';
+        if (Objects.equals(version.orElse(null), currentVersion)) {
+            return status(NOT_MODIFIED).build();
+        }
+
+        final var model = studentToModel(student, request);
+        log.debug("getById: model={}", model);
+        return ok().eTag(currentVersion).body(model);
+    }
+
+    private StudentModel studentToModel(final Student student, final HttpServletRequest request) {
         final var model = new StudentModel(student);
-        // evtl. Forwarding von einem API-Gateway
         final var baseUri = uriHelper.getBaseUri(request).toString();
         final var idUri = baseUri + '/' + student.getId();
+
         final var selfLink = Link.of(idUri);
         final var listLink = Link.of(baseUri, LinkRelation.of("list"));
         final var addLink = Link.of(baseUri, LinkRelation.of("add"));
         final var updateLink = Link.of(idUri, LinkRelation.of("update"));
         final var removeLink = Link.of(idUri, LinkRelation.of("remove"));
         model.add(selfLink, listLink, addLink, updateLink, removeLink);
-
-        log.debug("getById: {}", id);
         return model;
     }
 }
