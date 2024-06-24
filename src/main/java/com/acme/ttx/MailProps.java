8472e3463ffc07de1183c65d20a19fd9f71088d8
thanhxt <thanhtran_xuan@hotmail.de>
2024-06-24 18:37:55 +0200

    Spring configuration property app.mail

---
+++ src/main/java/com/acme/ttx/MailProps.java
@@ -0,0 +1,18 @@
+package com.acme.ttx;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.bind.DefaultValue;
+
+
+/**
+ * Spring-Konfiguration für Properties "app.mail.*".
+ *
+ * @param from E-Mail-Adresse des Absenders
+ * @param sales E-Mail-Adresse des Vertriebs
+ */
+@ConfigurationProperties(prefix = "app.mail")
+public record MailProps(
+    @DefaultValue("Theo Test <theo@test.de>")
+    String from,
+
+    @DefaultValue("Maxi Musterfrau <maxi.musterfrau@test.de>") String sales) {
