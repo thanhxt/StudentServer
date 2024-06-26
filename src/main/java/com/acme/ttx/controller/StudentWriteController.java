f771b651854b597ee48df8f24f8075dd7accf099
thanhxt <thanhtran_xuan@hotmail.de>
2024-06-26 16:37:16 +0200

    added new WriteController

--- src/main/java/com/acme/ttx/controller/StudentWriteController.java
+++ src/main/java/com/acme/ttx/controller/StudentWriteController.java
@@ -1,18 +1,14 @@
 package com.acme.ttx.controller;
 
 import com.acme.ttx.controller.StudentDTO.OnCreate;
-import com.acme.ttx.service.EmailExistsException;
-import com.acme.ttx.service.StudentWriteServiceOld;
+import com.acme.ttx.service.StudentWriteService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.ConstraintViolationException;
 import jakarta.validation.Valid;
 import jakarta.validation.groups.Default;
-import java.net.URI;
-import java.util.UUID;
 import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
@@ -23,14 +19,24 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Optional;
+import java.util.UUID;
+
 import static com.acme.ttx.controller.StudentGetControllerOld.ID_PATTERN;
-import static com.acme.ttx.controller.StudentGetControllerOld.REST_PATH;
 import static org.springframework.http.HttpStatus.NO_CONTENT;
+import static org.springframework.http.HttpStatus.PRECONDITION_FAILED;
+import static org.springframework.http.HttpStatus.PRECONDITION_REQUIRED;
 import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.http.ResponseEntity.badRequest;
 import static org.springframework.http.ResponseEntity.created;
+import static org.springframework.http.ResponseEntity.noContent;
 
 /**
  * Eine Controller-Klasse bildet die REST-Schnittstelle, wobei die HTTP-Methoden, Pfade und MIME-Typen auf die
@@ -38,50 +44,54 @@ import static org.springframework.http.ResponseEntity.created;
  * <img src="../../../../../../../extras/doc/StudentWriteController.png" alt="Klassendiagramm">
  */
 @Controller
-@RequestMapping(REST_PATH)
+@RequestMapping
+@Validated
 @RequiredArgsConstructor
 @Slf4j
-@SuppressWarnings({"ClassFanOutComplexity", "java:S1075", "java:S6856"})
-class StudentWriteController {
-    private static final String PROBLEM_PATH = "/problem/";
-    private final StudentWriteServiceOld service;
+public class StudentWriteController {
+    public static final String PROBLEM_PATH = "/problem/";
+
+    private static final String VERSIONSNUMMER_FEHLT = "Versionsnummer fehlt";
+
+    private final StudentWriteService service;
     private final StudentMapper mapper;
     private final UriHelper uriHelper;
 
-    /**
-     * Einen neuen Studenten-Datensatz anlegen.
-     *
-     * @param studentDTO Das Studentenobjekt aus dem eingegangenen Request-Body.
-     * @param request Das Request-Objekt, um 'Location' im Response-Header zu erstellen.
-     * @return Response mit Statuscode 201 einschließlich Location-Header oder
-     *         Statuscode 422, falls Constraints verletzt
-     *         sind oder die E-Mail-Adresse bereits existiert oder Statuscode
-     *         400, falls syntaktische Fehler im Request-Body
-     *         vorliegen.
-     */
-    @SneakyThrows
+
     @PostMapping(consumes = APPLICATION_JSON_VALUE)
-    @Operation(summary = "Einen neuen Studenten anlegen", tags = "Neuanlegen")
-    @ApiResponse(responseCode = "201", description = "Studenten neu angelegt")
+    @Operation(summary = "Einen neuen Kunden anlegen", tags = "Neuanlegen")
+    @ApiResponse(responseCode = "201", description = "Kunde neu angelegt")
     @ApiResponse(responseCode = "400", description = "Syntaktische Fehler im Request-Body")
     @ApiResponse(responseCode = "422", description = "Ungültige Werte oder Email vorhanden")
+    @SuppressWarnings("TrailingComment")
     ResponseEntity<Void> post(
-        @RequestBody @Validated ({Default.class, OnCreate.class})final StudentDTO studentDTO,
-        final HttpServletRequest request) {
-        log.debug("post: {}", studentDTO);
+        @RequestBody @Validated({Default.class, OnCreate.class}) final StudentDTO studentDTO,
+        final HttpServletRequest request
+    ) throws URISyntaxException {
+        log.debug("post: studentDTO={}", studentDTO);
+
+        if (studentDTO.username() == null || studentDTO.password() == null) {
+            return badRequest().build();
+        }
 
         final var studentInput = mapper.toStudent(studentDTO);
-        final var studentDB = service.create(studentInput);
-        final var baseUri = uriHelper.getBaseUri(request).toString();
-        final var location = URI.create(baseUri + '/' + studentDB.getId());
+        final var student = service.create(studentInput);
+        final var baseUri = uriHelper.getBaseUri(request);
+        final var location = new URI(baseUri.toString() + '/' + student.getId());
         return created(location).build();
     }
 
+
     /**
-     * Einen vorhandenen Studenten-Datensatz überschreiben.
+     * Einen vorhandenen Studenten
      *
      * @param id Matrikelnummer des zu aktualisierenden Studenten.
      * @param studentDTO Das Studentenobjekt aus dem eingegangenen Request-Body.
+     * @param version Versionsnummer aus dem Header If-Match
+     * @param request Das Request-Objekt, um ggf. die URL für Problemdetails zu ermitteln
+     * @return Response mit Statuscode 204 oder Statuscode 400, falls der JSON-Datensatz syntaktisch nicht korrekt ist
+     * oder 422 falls Constraints verletzt sind oder die Emailadresse bereits existiert
+     * oder 412 falls die Versionsnummer nicht ok ist oder 428 falls die Versionsnummer fehlt.
      */
     @PutMapping(path = "{id:" + ID_PATTERN + "}", consumes = APPLICATION_JSON_VALUE)
     @ResponseStatus(NO_CONTENT)
@@ -90,12 +100,52 @@ class StudentWriteController {
     @ApiResponse(responseCode = "400", description = "Syntaktische Fehler im Request-Body")
     @ApiResponse(responseCode = "404", description = "Student nicht vorhanden")
     @ApiResponse(responseCode = "422", description = "Ungültige Werte oder Email vorhanden")
-    void put(
+    @ApiResponse(responseCode = "428", description = VERSIONSNUMMER_FEHLT)
+    ResponseEntity<Void> put(
         @PathVariable final UUID id,
-        @RequestBody @Valid final StudentDTO studentDTO) {
-        log.debug("put: matrikelnummer={}, {}", id, studentDTO);
+        @RequestBody @Valid final StudentDTO studentDTO,
+        @RequestHeader("If-Match") final Optional<String> version,
+        final HttpServletRequest request
+    ) {
+        log.debug("put: id={}, studentDTO={}", id, studentDTO);
+        final int versionInt = getVersion(version, request);
         final var studentInput = mapper.toStudent(studentDTO);
-        service.update(studentInput, id);
+        final var student = service.update(studentInput, id, versionInt);
+        log.debug("put: student={}", student);
+        return noContent().eTag("\"" + student.getVersion() + '"').build();
+    }
+
+    private int getVersion(final Optional<String> versionOpt, final HttpServletRequest request) {
+        log.trace("getVersion: {}", versionOpt);
+        final var versionStr = versionOpt.orElseThrow(() -> new VersionInvalidException(
+            PRECONDITION_REQUIRED,
+            VERSIONSNUMMER_FEHLT,
+            URI.create(request.getRequestURL().toString())
+        ));
+        if (versionStr.length() < 3 ||
+            versionStr.charAt(0) != '"' ||
+            versionStr.charAt(versionStr.length() - 1) != '"') {
+            throw new VersionInvalidException(
+                PRECONDITION_FAILED,
+                "Ungültiges ETag " + versionStr,
+                URI.create(request.getRequestURL().toString())
+            );
+        }
+
+        final int version;
+        try {
+            version = Integer.parseInt(versionStr.substring(1, versionStr.length() - 1));
+        } catch (final NumberFormatException ex) {
+            throw new VersionInvalidException(
+                PRECONDITION_FAILED,
+                "Ungueltiges ETag " + versionStr,
+                URI.create(request.getRequestURL().toString()),
+                ex
+            );
+        }
+
+        log.trace("getVersion: version={}", version);
+        return version;
     }
 
     @ExceptionHandler
@@ -114,15 +164,5 @@ class StudentWriteController {
 
         return problemDetail;
     }
-
-    @ExceptionHandler
-    ProblemDetail onEmailExists(final EmailExistsException ex, final HttpServletRequest request) {
-        log.debug("onEmailExists: {}", ex.getMessage());
-        final var problemDetail = ProblemDetail.forStatusAndDetail(UNPROCESSABLE_ENTITY, ex.getMessage());
-        problemDetail.setType(URI.create(PROBLEM_PATH + ProblemType.CONSTRAINTS.getValue()));
-        problemDetail.setInstance(URI.create(request.getRequestURL().toString()));
-        return problemDetail;
-    }
-
 
 }
